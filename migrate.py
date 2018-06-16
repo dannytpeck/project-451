e0946db69487bc6dc0ef542df6c47b326be8427d
Danny Peck <dannytpeck@gmail.com>
2018-06-15 20:38:59 -0700

    Add csv output code

--- migrate.py
+++ migrate.py
@@ -1,14 +1,42 @@
 import json
 import requests
+import csv
 
-def get_one_page(page_number):
-    url = f'http://thelibrary.adurolife.com/wp-json/wp/v2/posts?page={page_number}'
+csvfile = "challenges.csv"
+
+headers = [
+    'Title',
+    'Instructions',
+    'More Information Html',
+    'Category',
+    'Limeade Dimensions',
+    'Limeade Image Url',
+    'Team Activity',
+    'Reward Occurrence',
+    'Activity Tracking Type',
+    'Activity Goal',
+    'Activity Goal Text',
+    'Device Enabled',
+    'Device Units',
+    'Header Image'
+]
+
+result = [headers]
+
+
+def get_one_page(page):
+    url = f'http://thelibrary.adurolife.com/wp-json/wp/v2/posts?page={page}'
     response = requests.get(url)
 
     if response.status_code == 200:
         return json.loads(response.content.decode('utf-8'))
     else:
         return None
+
+
+# # Library has 91 pages
+# for page_number in range(1, 91):
+#     get_one_page(page_number)
 
 first_page = get_one_page(1)
 
@@ -25,6 +53,9 @@ for post in first_page:
     print(date)
     print(modified)
 
+    # After putting it all in a list, push the list to the result list
+    # result.append(new_list)
+
     # Get image using the media endpoint
     image_id = post['featured_media']
     url = f'http://thelibrary.adurolife.com/wp-json/wp/v2/media/{image_id}'
@@ -33,6 +64,11 @@ for post in first_page:
         data = json.loads(response.content.decode('utf-8'))
         image_url = data['guid']['rendered']
         print(image_url)
+
+# Output result to CSV
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerows(result)
 
 # Notes for later
 
